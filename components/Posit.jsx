8612eba8f9e2e82f3dff5ece2bbd3a374a3b59f9
PedroOrtRec <pedro.ortega.dw@immune.institute>
2023-02-22 18:21:03 +0100

    estilos del componente Posit

--- components/Posit.jsx
+++ components/Posit.jsx
@@ -1,4 +1,20 @@
 import Image from "next/image"
+import styled from "styled-components"
+
+const StyledPosit = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+background-color: #ffffff1f;
+border-radius: 10px;
+box-shadow: black 1px 1px 5px;
+width: 200px;
+height: 400px;
+`
+
+const StyledImgContainer = styled.div`
+height: 50%;
+`
 
 export const Posit = ({ post }) => {
 
@@ -6,14 +22,14 @@ export const Posit = ({ post }) => {
     const imageAlt = imageTitle.replace("/images/", "")
     console.log(imageTitle)
 
-    return <div className="posit">
-        <div className="imagecontainer">
-            <Image src={post.imageTitle}
-                width={300}
-                height={300}
-                alt={imageAlt} />
-        </div>
+    return <StyledPosit>
+        <StyledImgContainer>
+            {/* <Image src={post.imageTitle}
+                fill
+                alt={imageAlt} /> */}
+            <img src={post.imageTitle} alt={imageAlt} width={200} />
+        </StyledImgContainer>
         <h3>{post.title}</h3>
         <p>{post.date}</p>
-    </div>
+    </StyledPosit>
 }
